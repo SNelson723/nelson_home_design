2c0060acd07e979a954f83905687c3702a3ba0d1
SNelson723 <stephenn649@gmail.com>
2024-09-05 18:49:02 -0500

    I realized I got rid of an environment variable that is needed so I put it back

--- src/server/main.js
+++ src/server/main.js
@@ -141,14 +141,7 @@ app.post('/sendEmail', async (req, res) => {
 // Start the server using ViteExpress
 // At the top of your server.js file
 const isProduction = process.env.NODE_ENV === 'production';
-let PORT;
-
-// You can then use this variable to make decisions in your code
-if (isProduction) {
-  PORT = process.env.PORT;
-} else {
-  PORT = 3000;
-}
+let PORT = isProduction ? process.env.PORT : 3000;
 
 ViteExpress.listen(app, PORT, () =>
   console.log(`Server is listening at ${PORT}`),
